c46cfae2d4520e20e48af3d830f729aa82928b64
Felipe Reis <felipe@glr.com.br>
2016-09-14 20:31:57 -0300

    adjustments to make functions runnable

--- mapper.py
+++ mapper.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
+import sys
 
-def mapper():
-    for line in sys.stdin:
-        data = line.strip().split("\t")
+for line in sys.stdin:
+    data = line.strip().split("\t")
 
-        if len(data) == 6:
-            date, time, store, item, cost, payment = data
-            print "{0}\t{1}".format(store, cost)
+    if len(data) == 6:
+        date, time, store, item, cost, payment = data
+        print "{0}\t{1}".format(store, cost)
